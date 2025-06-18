7ff6a3dda46f9c76730e9059937a0258783d8a32
Adrian Vera <vera254d4@gmail.com>
2025-06-17 21:41:14 -0500

    Ultimas modificaciones

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,99 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+	return (
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+		<div className="relative min-h-screen inter-font">
+
+
+			<div className="login-wrapper">
+
+				{/* Falta el responsive de aqui para abajo */}
+				<div className="w-md p-6 pb-8 bg-[#262626] bg-opacity-50 rounded-lg shadow-lg">
+
+					<div className="w-full">
+						<Image
+							className="mx-auto mb-3"
+							src="/ube-logo-texto.png"
+							alt="UBE-LOGO"
+							width={220}
+							height={45}
+							priority
+						/>
+					</div>
+
+					<div className="mb-5 mt-5">
+						<p className="text-center text-xl text-white inter-font font-bold">
+							MARKETPLACE UNIVERSITARIO
+						</p>
+					</div>
+
+
+					<form className="space-y-4 pl-5 pr-5 form-login">
+
+						<div className="input-group">
+							<label htmlFor="email">Correo electrónico</label>
+							<input
+								type="email"
+								name="email"
+								placeholder="Correo electrónico"
+								className="input-form"
+								autoComplete="off"
+							/>
+						</div>
+
+
+						<div className="input-group">
+							<label htmlFor="password">Contraseña</label>
+							<input
+								type="password"
+								name="password"
+								placeholder="Contraseña"
+								className="input-form"
+								autoComplete="off"
+							/>
+						</div>
+
+						<div className="input-group">
+							<a href="#" className="text-md">
+								¿Olvidaste tu contraseña?
+							</a>
+
+							<button
+								type="submit"
+								className="mt-2 w-full py-2 bg-[#e3132d] text-md text-white rounded hover:bg-[#BF0F27] transition cursor-pointer"
+							>
+								Iniciar sesión
+							</button>
+						</div>
+
+					</form>
+				</div>
+
+				<div className="w-md p-6 bg-[#262626] bg-opacity-50 rounded-lg shadow-lg">
+
+					<h2 className="text-center text-xl font-bold mb-4 text-[#e3132d]">
+						¿Aún no tienes cuenta?
+					</h2>
+					<p className="text-md text-center text-white">
+						Carreras de pre y post grado, cursos, diplomados, pasantías, etc. Todo en un solo lugar, para ti.
+					</p>
+
+					<button
+						type="submit"
+						className="mt-3 w-full py-2 bg-[#e3132d] text-md text-white rounded hover:bg-[#BF0F27] transition cursor-pointer"
+					>
+						Crear cuenta
+					</button>
+				</div>
+
+
+
+			</div>
+		</div>
+
+
+
+
+	);
 }
